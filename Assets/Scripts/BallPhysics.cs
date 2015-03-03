5e2e6e84b140cfa7ede692c361ea39d0f4581753
andela-gukpere <godson.ukpere@andela.co>
2015-03-03 01:04:15 +0100

    Initial WIP

---
+++ Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallPhysics : MonoBehaviour {
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		Transform gameObj = collision.transform;
+		Vector3 force;
+		if (gameObj.tag == "walls") {
+			force = transform.position;
+			if(gameObj.name == "wallz") {
+				force.Set(force.x, force.y, force.z * (-2f));
+				Debug.Log("vertical");
+			}
+			else {
+				force.Set(force.x * (-2f), force.y, force.z);
+			}
+			//transform.rigidbody.AddExplosionForce(50000f, force, 10f, 0f, ForceMode.Impulse);
+			transform.rigidbody.AddForce(force);
+			Debug.Log(force.ToString() + " Force");
+		}
+		else if(gameObj.tag == "Player") {
+			force = transform.position;
+			force.Set(force.x, force.y, force.z * (-4f));
+			//transform.rigidbody.AddExplosionForce(100000f, force, 10f, 0f, ForceMode.Impulse);
+			//Debug.Log(force.ToString() + " Force");
+			transform.rigidbody.AddForce(force);
+		}
+	}
+
+	void Start () {
+		transform.rigidbody.AddForce(new Vector3(transform.position.x, transform.position.y, transform.position.z * 3f));
+
+	}
+}
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerControls : MonoBehaviour {
+
+	public float forceMultiplier = 1000f;
+	
+	void playerPositionFix(float horizontal, float vertical) {
+
+		switch((int)vertical) {
+		case -1:
+			if(transform.position.z < -4.8f) {
+				transform.position = new Vector3(transform.position.x, transform.position.y, -4.7f);
+				Debug.Log("Reset -z");
+			}
+			break;
+		case 1:
+			if(transform.position.z > 4.8f) {
+				transform.position = new Vector3(transform.position.x, transform.position.y, 4.7f);
+				Debug.Log("Reset +z");
+			}
+			break;
+		}
+
+		switch((int)horizontal) {
+		case -1:
+			if(transform.position.x < -3.9f) {
+				transform.position = new Vector3(-3.8f, transform.position.y, transform.position.z);
+				Debug.Log("Reset -x");
+			}
+			break;
+		case 1:
+			if(transform.position.x > 3.9f) {
+				transform.position = new Vector3(3.8f, transform.position.y, transform.position.z);
+				Debug.Log("Reset +x");
+			}
+			break;
+		}
+
+	}
+
+	void Update () {
+		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+		//Vector3 position = transform.position;
+		if (vertical != 0) {
+			vertical = vertical > 0 ? 1f : -1f;
+			transform.rigidbody.AddForce(Vector3.forward * vertical * forceMultiplier);
+			//position = new Vector3(position.x, position.y, position.z + vertical);		
+		}
+
+		if (horizontal != 0) {
+			horizontal = horizontal > 0 ? 1f : -1f;
+			transform.rigidbody.AddForce(Vector3.right * horizontal * forceMultiplier);
+			//position = new Vector3(position.x + horizontal, position.y, position.z);		
+		}
+
+		playerPositionFix (horizontal, vertical);
+		//z-4.8
+		//x-3.8
+		//Debug.Log (transform.position.ToString ()  +" (x,y,z)");
+		//transform.position = position;
+	}
+}
